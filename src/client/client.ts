119046fda1792d590b99e4cac6e9b8cf7ef8ff56
Alejandro Medina Robles <alejandro.medinar@agilethought.com>
2023-03-29 16:01:42 -0600

    Add stats

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-
+import Stats from 'three/examples/jsm/libs/stats.module';
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(
@@ -35,18 +35,22 @@ function onWindowResize() {
     render()
 }
 
-// function animate() {
-//     requestAnimationFrame(animate)
+const stats = Stats()
+document.body.appendChild(stats.dom)
 
-//     cube.rotation.x += 0.01
-//     cube.rotation.y += 0.01
+ function animate() {
+     requestAnimationFrame(animate)
 
-//     render()
-// }
+     //cube.rotation.x += 0.01
+     //cube.rotation.y += 0.01
+
+     // render()
+     stats.update()
+ }
 
 function render() {
     renderer.render(scene, camera)
 }
 
-//animate()
+animate()
 render()
