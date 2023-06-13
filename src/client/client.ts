5ced21972cd06a626cf769c26dd70de07dbd37ed
Alejandro Medina <5almero5@gmail.com>
2023-06-13 09:42:21 -0600

    Add Textures and refractions

--- src/client/client.ts
+++ src/client/client.ts
@@ -26,8 +26,14 @@ const icosahedronGeometry = new THREE.IcosahedronGeometry(1, 0)
 const planeGeometry = new THREE.PlaneGeometry()
 const torusKnotGeometry = new THREE.TorusKnotGeometry()
 
-const material = new THREE.MeshBasicMaterial()
-//const material= new THREE.MeshNormalMaterial()
+const material = new THREE.MeshBasicMaterial()// { color: 0x00ff00, wireframe: true } )
+
+// const texture = new THREE.TextureLoader().load("img/grid.png")
+// material.map = texture
+const envTexture = new THREE.CubeTextureLoader().load(["img/px_50.png", "img/nx_50.png", "img/py_50.png", "img/ny_50.png", "img/pz_50.png", "img/nz_50.png"])
+ // envTexture.mapping = THREE.CubeReflectionMapping
+envTexture.mapping = THREE.CubeRefractionMapping
+material.envMap = envTexture
 
 const cube = new THREE.Mesh(boxGeometry, material)
 cube.position.x = 5
@@ -66,13 +72,16 @@ const options = {
         BackSide: THREE.BackSide,
         DoubleSide: THREE.DoubleSide,
     },
+    combine: {
+        MultiplyOperation: THREE.MultiplyOperation,
+        MixOperation: THREE.MixOperation,
+        AddOperation: THREE.AddOperation,
+    },
 }
 
 const gui = new GUI()
 const materialFolder = gui.addFolder('THREE.Material')
-materialFolder
-    .add(material, 'transparent')
-    .onChange(() => (material.needsUpdate = true))
+materialFolder.add(material, 'transparent').onChange(() => material.needsUpdate = true)
 materialFolder.add(material, 'opacity', 0, 1, 0.01)
 materialFolder.add(material, 'depthTest')
 materialFolder.add(material, 'depthWrite')
@@ -85,8 +94,22 @@ materialFolder
     .onChange(() => updateMaterial())
 materialFolder.open()
 
+const data = {
+    color: material.color.getHex(),
+}
+
+const meshBasicMaterialFolder = gui.addFolder('THREE.MeshBasicMaterial')
+meshBasicMaterialFolder.addColor(data, 'color').onChange(() => { material.color.setHex(Number(data.color.toString().replace('#', '0x'))) })
+meshBasicMaterialFolder.add(material, 'wireframe')
+//meshBasicMaterialFolder.add(material, 'wireframeLinewidth', 0, 10)
+meshBasicMaterialFolder.add(material, 'combine', options.combine).onChange(() => updateMaterial())
+meshBasicMaterialFolder.add(material, 'reflectivity', 0, 1)
+meshBasicMaterialFolder.add(material, 'refractionRatio', 0, 1)
+meshBasicMaterialFolder.open()
+
 function updateMaterial() {
     material.side = Number(material.side) as THREE.Side
+    material.combine = Number(material.combine) as THREE.Combine
     material.needsUpdate = true
 }
 
