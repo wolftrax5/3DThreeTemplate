2dc6adabae1a4d9bdd5e80980f4f4e39aab96a50
Alejandro Medina <5almero5@gmail.com>
2023-06-13 09:43:07 -0600

    Final Texture

--- src/client/client.ts
+++ src/client/client.ts
@@ -26,12 +26,19 @@ const icosahedronGeometry = new THREE.IcosahedronGeometry(1, 0)
 const planeGeometry = new THREE.PlaneGeometry()
 const torusKnotGeometry = new THREE.TorusKnotGeometry()
 
-const material = new THREE.MeshBasicMaterial()// { color: 0x00ff00, wireframe: true } )
+const material = new THREE.MeshBasicMaterial() //{ color: 0x00ff00, wireframe: true })
 
-// const texture = new THREE.TextureLoader().load("img/grid.png")
-// material.map = texture
-const envTexture = new THREE.CubeTextureLoader().load(["img/px_50.png", "img/nx_50.png", "img/py_50.png", "img/ny_50.png", "img/pz_50.png", "img/nz_50.png"])
- // envTexture.mapping = THREE.CubeReflectionMapping
+const texture = new THREE.TextureLoader().load('img/grid.png')
+material.map = texture
+const envTexture = new THREE.CubeTextureLoader().load([
+    'img/px_50.png',
+    'img/nx_50.png',
+    'img/py_50.png',
+    'img/ny_50.png',
+    'img/pz_50.png',
+    'img/nz_50.png',
+])
+//envTexture.mapping = THREE.CubeReflectionMapping
 envTexture.mapping = THREE.CubeRefractionMapping
 material.envMap = envTexture
 
@@ -99,10 +106,14 @@ const data = {
 }
 
 const meshBasicMaterialFolder = gui.addFolder('THREE.MeshBasicMaterial')
-meshBasicMaterialFolder.addColor(data, 'color').onChange(() => { material.color.setHex(Number(data.color.toString().replace('#', '0x'))) })
+meshBasicMaterialFolder.addColor(data, 'color').onChange(() => {
+    material.color.setHex(Number(data.color.toString().replace('#', '0x')))
+})
 meshBasicMaterialFolder.add(material, 'wireframe')
 //meshBasicMaterialFolder.add(material, 'wireframeLinewidth', 0, 10)
-meshBasicMaterialFolder.add(material, 'combine', options.combine).onChange(() => updateMaterial())
+meshBasicMaterialFolder
+    .add(material, 'combine', options.combine)
+    .onChange(() => updateMaterial())
 meshBasicMaterialFolder.add(material, 'reflectivity', 0, 1)
 meshBasicMaterialFolder.add(material, 'refractionRatio', 0, 1)
 meshBasicMaterialFolder.open()
